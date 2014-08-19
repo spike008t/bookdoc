130eba6cc78be8a0ab6d2734c27a4c619b526a0b
Tieu-Philippe KHIM <khim.tieu.philippe@gmail.com>
2014-08-20 00:39:11 +0200

    #ui on minimize go to tray

--- app/app/Application.js
+++ app/app/Application.js
@@ -5,14 +5,30 @@
  */
 Ext.define('BookDoc.Application', {
     extend: 'Ext.app.Application',
-    
+
     name: 'BookDoc',
 
     stores: [
         // TODO: add global / shared stores here
     ],
-    
+
     launch: function () {
         // TODO - Launch the application
+        var gui = require('nw.gui');
+        var win = gui.Window.get();
+        var tray;
+
+        // tray management
+
+        win.on('minimize', function() {
+           this.hide();
+           tray = new gui.Tray({icon: 'icon.png'});
+
+           tray.on('click', function() {
+             win.show();
+             this.remove();
+             tray = null;
+          });
+        });
     }
 });
